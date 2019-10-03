c5898e3ca1365f7eff72cfdab4dd2df8961d34f9
raghu prasad <prasadraghuks@gmail.com>
2019-10-03 10:45:41 +0530

    first commit

---
+++ JS/allCoursePageScript.js
@@ -0,0 +1,83 @@
+//javascript for index Page
+
+
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+
+
+
+
+// Modal function
+function modalBox() {
+// Get the modal
+var modal1 = document.getElementById("id01");
+var modal2 = document.getElementById("id02");
+
+
+
+// Get the button that opens the modal
+var span = document.getElementById("openSignup");
+
+
+	
+// When the user clicks on the button, open the modal 
+span.onclick = function() {
+  modal1.style.display = "block";
+}
+
+ modal2.style.display = "none";
+
+}
+
+
+
+
+
+//for footer
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+  });
+})
+
